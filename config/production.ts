9bd1905e42f208e25756a8e7aa709d1a395d9859
reyamanso2023 <124692784+reyamanso2023@users.noreply.github.com>
2024-05-22 11:08:02 -0400

    Update production.ts

--- config/production.ts
+++ config/production.ts
@@ -6,7 +6,7 @@ const config: ConfigInterface = {
 // Moonbeam
 // const config: ConfigInterface = {
 //   CHAIN_ID: '0x504',
-//   CONTRACT_ADDRESS: '',
+//   CONTRACT_ADDRESS: '0x5b29a02C75d0f0c9739Daa3d12733D810c285e42',
 // };
 // Astar
 // const config: ConfigInterface = {
